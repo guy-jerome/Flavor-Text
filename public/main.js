edc747cf75d388f894c026f9cf4781f67c8e1470
guy-jerome <aaronjamesroberts@gmail.com>
2023-11-16 22:26:03 -0600

    Adds the other pages

--- public/main.js
+++ public/main.js
@@ -1,58 +1,82 @@
+//Menu components
+const $menu = $('#menu')
+const $goWorldBtn = $('#go-world-btn')
+const $goAreaBtn = $("#go-area-btn")
+const $goLocationBtn = $("#go-location-btn")
+//World components
 const $world = $('#world');
 const $worldName = $('#world-name');
 const $worldDescription = $('#world-description');
 const $worldBtn = $('#world-btn');
 const $worldFullDescription = $('#world-full-description');
-const $saveBtn = $('#save-btn')
-const $select = $('#select')
-const $selectWorldBtn = $("#select-world-btn")
+const $worldSaveBtn = $('#world-save-btn')
+const $worldSelect = $('#world-select')
+const $worldSelectBtn = $("#world-select-btn")
+//Area components
+const $area = $('#area');
+const $areaName = $('#area-name');
+const $areaDescription = $('#area-description');
+const $areaBtn = $('#area-btn');
+const $areaFullDescription = $('#area-full-description');
+const $areaSaveBtn = $('#area-save-btn')
+const $areaSelect = $('#area-select')
+const $areaSelectBtn = $("#area-select-btn")
+//Location components
+const $location = $('#location');
+const $locationName = $('#location-name');
+const $locationDescription = $('#location-description');
+const $locationBtn = $('#location-btn');
+const $locationFullDescription = $('#location-full-description');
+const $locationSaveBtn = $('#location-save-btn')
+const $locationSelect = $('#location-select')
+const $locationSelectBtn = $("#location-select-btn")
 
-const socket = io()
-let page = "world"
+let page = "menu"
 
-async function getStream(){
-  $saveBtn.prop("disabled",true)
-  $worldBtn.prop("disabled",true)
-  const response = await fetch('/stream', {
-    method: "POST",
-    headers: {
-      'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify({
-      name: $worldName.val(),
-      simpledes: $worldDescription.val()
-    })
-  })
-  const data = response.json()
-  console.log(data)
-  $saveBtn.prop("disabled",false)
-  $worldBtn.prop("disabled",false)
+function navigateToMenu(){
+  page = "menu"
+  $world.hide()
+  $area.hide()
+  $location.hide()
+  $menu.show()
 }
-
-async function saveWorld(){
-  const response = await fetch('/streamsave', {
-    method: "POST",
-    headers: {
-      'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify({
-      name: $worldName.val(),
-      simpledes: $worldDescription.val(),
-      fulldes: $worldFullDescription.val()
-    })
-  })
-  const data = await response.json();
+function navigateToWorld(){
+  page = "world"
+  $menu.hide()
+  $area.hide()
+  $location.hide()
+  $world.show()
   getWorlds()
 }
-
-async function getWorlds(){
-  const response = await fetch('/worlds')
-  const data = await response.json();
-  for (let world of data){
-    const option = `<option value="${world.id}">${world.name}</option>`
-    $select.append(option)
-  }
+function navigateToArea(){
+  page = "area"
+  $menu.hide()
+  $world.hide()
+  $location.hide()
+  $area.show()
 }
+function navigateToLocation(){
+  page = "location"
+  $menu.hide()
+  $world.hide()
+  $area.hide()
+  $location.show()
+}
+
+navigateToMenu()
+
+const socket = io()
+
+
+$goWorldBtn.on("click",navigateToWorld) 
+$goAreaBtn.on("click", navigateToArea)
+$goLocationBtn.on("click", navigateToLocation)
+
+
+$worldBtn.on("click", getStream)
+$worldSaveBtn.on("click", saveWorld)
+$worldSelectBtn.on("click", loadWorld)
+
 
 socket.on('stream-chunk', (chunk) => {
   if (page === "world"){
@@ -61,26 +85,82 @@ socket.on('stream-chunk', (chunk) => {
   }
 });
 
-$worldBtn.on("click", getStream)
-
-$saveBtn.on("click", ()=>{
-  saveWorld()
-})
-
-async function loadWorld(){
-  $worldName.val("")
-  $worldDescription.val("")
-  $worldFullDescription.val("")
-  const response = await fetch(`/worlds/${$select.val()}`)
-  const data = await response.json();
-  console.log(data)
-  $worldName.val(data[0].name)
-  $worldDescription.val(data[0].simpledes)
-  $worldFullDescription.val(data[0].fulldes)
+async function getStream() {
+  $worldSaveBtn.prop("disabled", true)
+  $worldBtn.prop("disabled", true)
+  $worldSelectBtn.prop("disabled", true)
+  try {
+    const response = await fetch('/stream', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: $worldName.val(),
+        simpledes: $worldDescription.val()
+      })
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching stream:', error);
+  } finally {
+    $worldSaveBtn.prop("disabled", false)
+    $worldBtn.prop("disabled", false)
+    $worldSelectBtn.prop("disabled", false)
+  }
 }
 
-$selectWorldBtn.on("click", ()=>{
- loadWorld()
-})
+async function saveWorld() {
+  try {
+    const response = await fetch('/streamsave', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: $worldName.val(),
+        simpledes: $worldDescription.val(),
+        fulldes: $worldFullDescription.val()
+      })
+    });
 
-getWorlds()
+    const data = await response.json();
+    getWorlds();
+  } catch (error) {
+    console.error('Error saving world:', error);
+  }
+}
+
+async function getWorlds() {
+  try {
+    const response = await fetch('/worlds');
+    const data = await response.json();
+
+    $select.empty();
+
+    for (let world of data) {
+      const option = `<option value="${world.id}">${world.name}</option>`;
+      $select.append(option);
+    }
+  } catch (error) {
+    console.error('Error fetching worlds:', error);
+  }
+}
+
+async function loadWorld() {
+  try {
+    $worldName.val("");
+    $worldDescription.val("");
+    $worldFullDescription.val("");
+
+    const response = await fetch(`/worlds/${$worldSelect.val()}`);
+    const data = await response.json();
+
+    $worldName.val(data[0].name);
+    $worldDescription.val(data[0].simpledes);
+    $worldFullDescription.val(data[0].fulldes);
+  } catch (error) {
+    console.error('Error loading world:', error);
+  }
+}
--- server.js
+++ server.js
@@ -1,114 +1,124 @@
-
-const OpenAI = require('openai');
-const dotenv = require('dotenv');
-const pool = require('./db.js');
-const { pipeline } = require('stream');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const { pipeline } = require('stream');
+const OpenAI = require('openai');
+const dotenv = require('dotenv');
+const pool = require('./db.js');
 
+// Load environment variables
+dotenv.config();
+
+// Express setup
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static("public"))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// Socket.IO setup
+setupSocketIO();
 
-  app.set('socket', socket);
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
-
-app.post('/stream', async (req, res)=>{
-  const {name, simpledes } = req.body
-  const fullDescription = await generateText(req,res,name,simpledes)
-  res.status(200).json({message: "stream complete"})
-})
-
-app.post('/streamsave', async (req, res)=>{
-  
-  const {name, simpledes,fulldes} = req.body
-  const data = await postWorld(name,simpledes,fulldes)
-  res.status(200).json({message:"world saved"})
-})
-
-app.get('/worlds', async (req,res)=>{
-  const data = await dataBaseQuery('SELECT name, id FROM worlds;')
-  res.status(200).json(data)
-})
-
-app.get('/worlds/:id', async (req,res)=>{
-  const data = await dataBaseQuery('SELECT * FROM worlds WHERE id = $1', [req.params.id])
-  res.status(200).json(data)
-})
+// Routes
+app.post('/stream', streamHandler);
+app.post('/streamsave', streamSaveHandler);
+app.get('/worlds', getAllWorldsHandler);
+app.get('/worlds/:id', getWorldByIdHandler);
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
-async function dataBaseQuery(query,args = []){
+// Database Functions
+async function dataBaseQuery(query, args = []) {
   const client = await pool.connect();
-  try{
-    const response = await client.query(query,args)
-    return response.rows
-  }catch (err){
-    console.error('Error executing database query:', err)
-    return err
-  }finally{
-    client.release()
+  try {
+    const response = await client.query(query, args);
+    return response.rows;
+  } catch (err) {
+    console.error('Error executing database query:', err);
+    throw err;
+  } finally {
+    client.release();
   }
 }
 
-
-async function postWorld(name,simpledes, fulldes) {
+async function postWorld(name, simpledes, fulldes) {
   const client = await pool.connect();
-  try{
-    const response = await client.query('INSERT INTO worlds (name, simpledes, fulldes) VALUES ($1,$2,$3) RETURNING *;',[name,simpledes,fulldes])
-    return response.rows
-  }catch (err){
-    return err
-  }finally{
-    client.release()
+  try {
+    const response = await client.query('INSERT INTO worlds (name, simpledes, fulldes) VALUES ($1,$2,$3) RETURNING *;', [name, simpledes, fulldes]);
+    return response.rows;
+  } catch (err) {
+    throw err;
+  } finally {
+    client.release();
   }
 }
 
-
+// OpenAI Setup
 const openai = new OpenAI({ apiKey: process.env.CHATGTP_API_KEY });
 
 const systemContent =
-  "You are a professional dungeon master and creative writer with over 50 years of experience.\
- You specialize in writing the flavor text for TTRPGS. Your work is extremely creative and artistic. You are helping write out the flavor text \
- for a new ttrpg module. Given the basic dungeon, city, or town layout, you are able to create a cohesive description of each room, building, and landmark.\
- You are precise and unique.";
+  "You are a professional dungeon master and creative writer with over 50 years of experience. \
+  You specialize in writing the flavor text for TTRPGS. Your work is extremely creative and artistic. \
+  You are helping write out the flavor text for a new ttrpg module. Given the basic dungeon, city, or town layout, \
+  you are able to create a cohesive description of each room, building, and landmark. \
+  You are precise and unique.";
 
+// Route Handlers
+async function streamHandler(req, res) {
+  const { name, simpledes } = req.body;
+  const fullDescription = await generateText(req, res, name, simpledes);
+  res.status(200).json({ message: "stream complete" });
+}
 
+async function streamSaveHandler(req, res) {
+  const { name, simpledes, fulldes } = req.body;
+  const data = await postWorld(name, simpledes, fulldes);
+  res.status(200).json({ message: "world saved" });
+}
+
+async function getAllWorldsHandler(req, res) {
+  const data = await dataBaseQuery('SELECT name, id FROM worlds;');
+  res.status(200).json(data);
+}
+
+async function getWorldByIdHandler(req, res) {
+  const data = await dataBaseQuery('SELECT * FROM worlds WHERE id = $1', [req.params.id]);
+  res.status(200).json(data);
+}
+
+// OpenAI Text Generation
 async function generateText(req, res, name, simpledes) {
-  const userMessage = `Create the description of a fantasy world with the name of ${name} using the basic description of ${simpledes}`
-  const socket = req.app.get('socket')
+  const userMessage = `Create the description of a fantasy world with the name of ${name} using the basic description of ${simpledes}`;
+  const socket = req.app.get('socket');
   const completionStream = await openai.chat.completions.create({
     messages: [
       { role: 'system', content: systemContent },
-      { role: 'user', content: userMessage},
+      { role: 'user', content: userMessage },
     ],
     model: 'gpt-3.5-turbo',
     stream: true,
   });
-  let text = ""
+  let text = "";
   for await (const chunk of completionStream) {
-    
-    if (chunk.choices[0].delta.content){
+    if (chunk.choices[0].delta.content) {
       socket.emit('stream-chunk', chunk.choices[0].delta.content);
-      text +=chunk.choices[0].delta.content
-    } 
+      text += chunk.choices[0].delta.content;
+    }
   }
-  return text
+  return text;
+}
+
+// Socket.IO Setup function
+function setupSocketIO() {
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+    app.set('socket', socket);
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  });
 }
